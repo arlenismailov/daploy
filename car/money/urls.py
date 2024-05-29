1fb5bb3a55035ced4dcdd754a4de3e1f7cdf8eb3
arlen <arlenismailov02@gmail.com>
2024-05-29 15:46:27 +0600

    foo deploy

--- car/money/urls.py
+++ car/money/urls.py
@@ -3,7 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
-
     path('carorspareparts/', CarorSparepartsView.as_view({'get': 'list', 'post': 'create'}), name='product_list'),
     path('products/', ProductViewSets.as_view({'get': 'list', 'post': 'create'}), name='product_list'),
 
